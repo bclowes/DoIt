8d164d69cef2aee2792f37d77936a1a1ece9f276
Brian R Clowes <clowes.brian@gmail.com>
2017-12-10 06:14:19 -0500

    Initial Commit

---
+++ Do It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do It
+//
+//  Created by Brian R Clowes on 2017-12-10.
+//  Copyright © 2017 Brian R Clowes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
